51b98ce6c32ff25d4d5c7e4578bf9365ad23feb3
yeo-li <parkseongyeol2110@gmail.com>
2025-10-27 17:53:19 +0900

    feat(RacingGame): 자동차 경주 기능 구현
    시도 횟수 만큼 경주를 진행한 뒤, 각 회차의 횟수를 저장합니다.

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,22 +1,37 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
 
     private final List<RacingCar> racingCars;
     private final int attemptCount;
+    private final List<String> roundResults;
 
     public RacingGame(List<RacingCar> racingCars, int attemptCount) {
 
         this.racingCars = racingCars;
         this.attemptCount = attemptCount;
+        this.roundResults = new ArrayList<>();
+    }
+
+    public List<String> getRoundResults() {
+        return roundResults;
     }
 
     public void moveAllCars() {
 
         for (RacingCar racingCar : racingCars) {
             racingCar.moveForward();
+        }
+    }
+
+    public void race() {
+
+        for (int i = 0; i < attemptCount; i++) {
+            moveAllCars();
+            roundResults.add(this.toString());
         }
     }
 
