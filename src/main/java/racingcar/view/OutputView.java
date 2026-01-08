d010f91e581881e957ab5aa0feb333d992076a6e
yeo-li <parkseongyeol2110@gmail.com>
2026-01-08 13:46:12 +0900

    feat(OutputView): 실행 결과 출력 시작 메세지 뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,10 @@ import racingcar.domain.Car;
 
 public class OutputView {
 
+    public void printStatusResultMessage() {
+        System.out.println("실행 결과");
+    }
+
     public void printRacingStatus(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.status());
