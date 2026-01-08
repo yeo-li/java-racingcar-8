d75911c7c4c6525ef5ca286f2b7f1f68542a3c5e
yeo-li <parkseongyeol2110@gmail.com>
2026-01-08 13:48:59 +0900

    feat(OutputView): 우승자 안내 문구 출력 뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -15,4 +15,15 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    public void printRacingWinners(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            if (i + 1 == winners.size()) {
+                break;
+            }
+            System.out.print(", ");
+        }
+    }
 }
